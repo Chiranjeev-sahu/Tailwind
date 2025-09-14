2ad26c0d60a11325a45c033fa637480a6bfcb5cc
Chiranjeev-sahu <chiranjeevsahu1904@gmail.com>
2025-09-14 20:41:32 +0530

    idk

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center rounded-lg bg-blue-500 p-4 text-white hover:bg-blue-600">
+        hi
+      </div>
+    </>
+  );
+}
+
+export default App;
